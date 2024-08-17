c579c93efa35904d8718f24068e73da2d9486348
Rotwall72 <olyfez@gmail.com>
2024-08-17 01:55:12 -0400

    utelizing web API

--- README.md
+++ README.md
@@ -1,27 +1,27 @@
-## What is Threatibox?
+## What is Threatmixer?
 
-ThreatBox is a webpage which will allow the user to play with and mix the various threat themes from the game Rain World. I was heavily inspired by Incredibox, a game where you take various sound elements and mash them together to make your own music. Thinking about how fun it was to expiriment with different sound combinations in Incredibox, I thought about how cool it'd be if you could mix and match the layers of a threat theme at will, sparking the idea for this project.
+Threatmixer is a webpage which will allow the user to play with and mix the various threat themes from the game Rain World. I was heavily inspired by Incredibox, a game where you take various sound elements and mash them together to make your own music. Thinking about how fun it was to expiriment with different sound combinations in Incredibox, I thought about how cool it'd be if you could mix and match the layers of a threat theme at will, sparking the idea for this project.
 
 ## Progress
 
-So far, a very basic framework has been implemented. Almost all key-features have been added, but the webpage is currently very user unfriendly. In the next update for this, I'd have added some more css to make it easier to use. But as it stands, the user can play and mute each layer individualy, as well as pause the song and stop/play every layer at once. Currently, it's basically crappy Incredibox minus any visuals; and that's exactly what I was aiming for with my first commit. 
+With this commit, instead of straight up using the audio elements to control the layers, I've converted those audio elements into AudioBufferSourceNodes. This was so that there would be less popping and crackling noises when loading the audio, as well as more advanced controls over the audio's behavior and attributes. I didn't really add anything mechanically for the user to play with, but I did add just a bit of brightness to make up for the lack of ui. Oh yeah and I also changed the name because this is really going to be more of a fan work of Rain World than Incredibox, and I don't want to have "ibox" in the name if nothing from that game is going to be featured in this webpage. This a working title, and will be changed once I can come up with something better.
 
 ## TODO:
 
 Descending in priority:
 
-1. Smooth auto-looping
-2. Restarting the song when no layers are active
-3. Individual layer and master volume settings
-4. Pre-picking layers to begin with before the song begins
-5. Timing feature, to only allow layers to be added on-beat
-6. Recording feature
-7. Exporting recordings
-8. Other region threat themes
-9. Styling and detailing
+1. Fix up the soloing
+2. Individual layer and master volume settings
+3. Pre-picking layers to begin with before the song begins
+4. Timing feature, to only allow layers to be added on-beat
+5. Recording feature
+6. Exporting recordings
+7. Other region threat themes
+8. Styling and detailing
     - Audio visualizer
     - Song progress bar
-10. Misc. and not totally necessary stuff
+    - General ui improvements
+9. Misc. and not totally necessary stuff
     - Modded region threat themes?
     - Secret combinations?
     - Auto play feature?
--- script.js
+++ script.js
@@ -3,10 +3,18 @@ const layerButton = document.getElementsByClassName("layer_button");
 const soloButton = document.getElementsByClassName("solo_button")
 const pauseButton = document.getElementById("pause_button");
 const playAllButton = document.getElementById("play_button");
+const pageUI = document.getElementById("page_ui");
+const loadingScreen = document.getElementById("loading_screen");
 
-// variables to keep track of the song state
-let songPlaying = false;
-let hasBeenSoloed = false;
+// only showing the loading screen until the page is ready
+pageUI.style.display = "none";
+
+// global variables
+let loadSounds;
+let layerSoloed = false;
+let songStarted = false;
+let songPaused = false;
+let loadedLayers = [];
 let layerPlaying = [];
 
 // this array is hardcoded to just store the threat layers for outskirts, but will 
@@ -22,120 +30,166 @@ const SUThreatLayer = [
     new Audio("threat_music/SU/TH_SU - NOISE.mp3"),
 ]
 
-// connecting our audio files to the web API
-const typeOfContext = new (window.AudioContext || window.webkitAudioContext);
-SUThreatLayer.forEach((audio) => {
-    var layer = typeOfContext.createMediaElementSource(audio)
-    layer.connect(typeOfContext.destination);
-});
+// grabbing the audio context and establishing a GainNode
+const audioContext = new (window.AudioContext || window.webkitAudioContext);
 
-// this function stores the methods for how we will sync the layers together
-function prepLayers() {
-    SUThreatLayer.forEach((audio) => {
-        
-        // this if exists for the play all button
-        if (!songPlaying) {audio.muted = true;}
-        else {audio.muted = false;}
-        audio.volume = 0.5;
-        audio.play();
-
-        // setting the playback time for each of the audio files to ensure that they're all synced
-        audio.currentTime = 0;
-        playAllButton.innerText = "End All"
-    });
+// more web API junk
+// this is all so that the audio will loop smoothly
+try {
+    loadSounds = 
+        SUThreatLayer.map((audio) => { //personal note: .map is like .forEach except it turns each item into a promise, basically
+            return fetch(audio.src)
+                .then((result) => {return result.arrayBuffer();}) // turning the audio into a buffer
+                .then((arrayBuffer) => {return audioContext.decodeAudioData(arrayBuffer);}) // decoding that buffer
+        });
+}
+catch {
+    alert("Failed to load assets. Please refresh the page and try again.")
 }
 
-// using a for loop to check each button for inputs
-for (let i = 0; i < layerButton.length; i++) {
-    // listening for if a layer button has been clicked
-    layerButton[i].addEventListener("click", () => {
-        if (!songPlaying) {
-            prepLayers();
-            songPlaying = true;
-        }
+// we wait for all of the sounds to be buffered, then proceed
+Promise.all(loadSounds).then((audioBuffer) => {
+    loadingScreen.style.display = "none";
+    pageUI.style.display = "flex";
 
-        // muting and unmuting audio
-        if (!hasBeenSoloed) {
-            if (SUThreatLayer[i].muted) {
-                SUThreatLayer[i].muted = false;
-                layerPlaying.push(SUThreatLayer[i]);
-            }
-            else {
-                SUThreatLayer[i].muted = true;
-                var indexOfLayer = layerPlaying.indexOf(SUThreatLayer[i])
-                layerPlaying.splice(indexOfLayer, 1);
-            }
-        }
-    })
+    // this function stores the methods for how the layers will be set up
+    function prepLayers() {
+        // creating an AudioBufferSourceNode each time this function is called
+        audioBuffer.forEach((audioBuffer, index) => {
+            // creating the bufferSource
+            var bufferSource = audioContext.createBufferSource();
+            bufferSource.buffer = audioBuffer
+            bufferSource.loop = true;
+            
+            // connecting it to a gainNode
+            var gainNode = audioContext.createGain();
+            gainNode.connect(audioContext.destination);
+            bufferSource.connect(gainNode);
 
-    // listening for if a solo button has been clicked
-    soloButton[i].addEventListener("click", () => {
-        if (!hasBeenSoloed) {
-            for (let j = 0; j < layerPlaying.length; j++) {
-                if (layerPlaying[j] != SUThreatLayer[i]) {
-                    layerPlaying[j].muted = true;
+            // returning the buffer and the gain in a pair
+            loadedLayers.push([bufferSource, gainNode, index]);
+        });
+
+        for (let i = 0; i < loadedLayers.length; i++) {
+            if (!songStarted) {loadedLayers[i][1].gain.value = 0;} // if the play all button wasn't clicked, start all of the layers muted
+            else {loadedLayers[i][1].gain.value = 0.75;} // otherwise, let them all make sound
+            loadedLayers[i][0].start(audioContext.currentTime);
+        }
+        playAllButton.innerText = "End All"
+    }
+
+    // using a for loop to check each button for inputs
+    for (let i = 0; i < layerButton.length; i++) {
+        // listening for if a layer button has been clicked
+        layerButton[i].addEventListener("click", () => {
+            if (!songStarted) {
+                prepLayers();
+                songStarted = true;
+            }
+
+            // muting and unmuting audio
+            if (!layerSoloed) {
+                if (loadedLayers[i][1].gain.value == 0) {
+                    loadedLayers[i][1].gain.value = 0.75;
+                    layerPlaying.push(loadedLayers[i]);
+                    layerButton[i].style.filter = "brightness(100%)"
                 }
                 else {
-                    if (layerPlaying[j].muted = true) {
-                        layerPlaying[j].muted = false;
-                    }
+                    loadedLayers[i][1].gain.value = 0
+                    var indexOfLayer = layerPlaying.indexOf(loadedLayers[i])
+                    layerPlaying.splice(indexOfLayer, 1);
+                    layerButton[i].style.filter = "brightness(20%)"
                 }
-            }
-            hasBeenSoloed = true;
-        }
-        else {
-            for (let j = 0; j < layerPlaying.length; j++) {
-                if (layerPlaying[j] != SUThreatLayer[i]) {
-                    layerPlaying[j].muted = false;
-                }
-            }
-            hasBeenSoloed = false;
-        }
-    })
-}
+           }
+        })
 
-// pause button functionality
-pauseButton.addEventListener("click", () => {
-    SUThreatLayer.forEach((audio) => {
-        if (songPlaying) {
-            if (!audio.paused) {
-                audio.pause();
+        // listening for if a solo button has been clicked
+        soloButton[i].addEventListener("click", () => {
+            try {
+                if (!layerSoloed && loadedLayers[i][1].gain.value != 0) {
+                    for (let j = 0; j < layerPlaying.length; j++) {
+                        if (layerPlaying[j] != loadedLayers[i]) { // if the layer doesn't match the layer we're trying to mute,
+                            layerPlaying[j][1].gain.value = 0; // mute it
+                            layerButton[layerPlaying[j][2]].style.filter = "brightness(20%)"
+                        }
+                        else {
+                            soloButton[layerPlaying[j][2]].style.filter = "brightness(100%)"
+                        }
+
+                        /*
+                        // nullifying this if statement for now, I'll fix it on the next push
+                        else if (layerPlaying[j][1].gain.value == 0) { // if the layer we're trying to solo is muted,
+                            loadedLayers[j][1].gain.value == 0.75; // unmute it
+                            layerButton[loadedLayers[j][2]].style.filter = "brightness(100%)"
+                        }
+                        */
+                    }
+                    layerSoloed = true;
+                }
+
+                // this if handles un-soloing the song
+                else if (loadedLayers[i][1].gain.value != 0) {
+                    for (let j = 0; j < layerPlaying.length; j++) {
+                        layerPlaying[j][1].gain.value = 0.75;
+                        layerButton[layerPlaying[j][2]].style.filter = "brightness(100%)"
+                        soloButton[layerPlaying[j][2]].style.filter = "brightness(20%)"
+                    }
+                    
+                    layerSoloed = false;
+                }
+            }
+            catch {
+                // note, probably should hide solo buttons before sounds are loaded to prevent errors
+                console.error(err)
+            }
+        })
+    }
+
+    // pause button functionality
+    pauseButton.addEventListener("click", () => {
+        if (songStarted) {
+            if (!songPaused) {
+                audioContext.suspend();
+                songPaused = true;
                 pauseButton.innerText = "Unpause";
             }
+
             else {
-                audio.play();
+                audioContext.resume();
+                songPaused = false;
                 pauseButton.innerText = "Pause";
             }
         }
     });
-});
 
-// play all button functionality
-playAllButton.addEventListener("click", () => {
-    if (!songPlaying) {
-            songPlaying = true;
+    // play all button functionality
+    playAllButton.addEventListener("click", () => {
+        if (!songStarted) { // if we're trying to start all of them,
+            songStarted = true;
             prepLayers();
-            for (let i = 0; i < SUThreatLayer.length; i++) {
-                layerPlaying.push(SUThreatLayer[i])
+
+            // updating the layerPlaying array and brightness of the buttons
+            for (let i = 0; i < loadedLayers.length; i++) {
+                layerPlaying.push(loadedLayers[i]);
+                layerButton[i].style.filter = "brightness(100%)";
             }
+
             playAllButton.innerText = "End All"
         }
-    
-    else { 
-        SUThreatLayer.forEach(audio => {
-            audio.pause();
-            audio.currentTime = 0;
+        
+        else { // if we're trying to end all of them,
+            for (let i = 0; i < loadedLayers.length; i++) { // clearing AudioBufferSourceNodes
+                loadedLayers[i][0].stop();
+                loadedLayers[i][0].disconnect();
+                layerButton[i].style.filter = "brightness(20%)"
+            }
+
+            // reseting variables and button text
+            songStarted = false;
+            loadedLayers = [];
+            layerPlaying = [];
             playAllButton.innerText = "Play All"
             pauseButton.innerText = "Pause"
-            songPlaying = false;
-        });
-        layerPlaying = [];
-    }
+        }
+    });
 });
-
-// looping the song (still working on seamlessy looping it)
-// we check the last audio file in the array, since that would most likely be one the finish last
-SUThreatLayer[SUThreatLayer.length - 1].addEventListener("ended", () => {
-    console.log("Song ended")
-    prepLayers();
-})
